65c18de831e3606197ae401a742cf8655c723c7e
feylon <jamshid14092002@gmail.com>
2025-10-08 23:56:01 +0500

    feat: add role-based access control to Department controller

--- back/src/Department/Department.controller.ts
+++ back/src/Department/Department.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { DepartmanetService } from "./Department.service";
 import { DepartmentDTO, EditDepartmentDTO } from "./entity/DepartmentDTO";
 import { UUIDDTO } from "GlobalTypes/GlobalDTO";
+import { RolesGuard } from "src/rolecheck/rolecheck.guard";
+import { Roles } from "src/Role/types/roles.decorator";
+import { ERoles } from "src/Role/types/TypeRoles";
 
+@UseGuards(RolesGuard)
 @Controller("department")
 export class DepartmentController {
     constructor(private DepartmentService: DepartmanetService) { }
 
+    @Roles(ERoles.Superadmin)
     @Post('/createDepartment')
     createDepartment(@Body() body: DepartmentDTO) {
         return this.DepartmentService.createDepartment(body)
     }
 
-
+    @Roles(ERoles.Superadmin)
     @Put('/editDepartment/:id')
-    editDepartment(@Body() body : EditDepartmentDTO, @Param() param : UUIDDTO){
+    editDepartment(@Body() body: EditDepartmentDTO, @Param() param: UUIDDTO) {
         return this.DepartmentService.editDepartment(param, body)
     }
 }
